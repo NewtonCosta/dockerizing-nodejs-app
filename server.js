ec4373a8e288010b8e15049a48bfc89e7f764327
NewtonCosta <newtongomez04@gmail.com>
2021-03-08 01:21:38 +0100

    Added port and host ip variables

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-function handler(signal) {
-    console.log(`Received event: ${signal}`)
-}
-process.on('SIGHUP', handler)
+
 
 // Require framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const PORT = 3000
+const HOST = '0.0.0.0'
+
+// Function tp handle signal
+function handler(signal) {
+    console.log(`Received event: ${signal}`)
+}
+process.on('SIGTERM', handler)
 
 // Declare a route
 fastify.get('/', async(request, apply) => {
@@ -14,7 +19,7 @@ fastify.get('/', async(request, apply) => {
 // Run the server
 const start = async () => {
     try {
-        await fastify.listen(3000)
+        await fastify.listen(PORT, HOST)
         console.log(`Server running: Process pid: ${process.pid}`)
     }catch(err) {
         fastify.log.error(err);
